1dcf1157fcf7bd82abd129ce31633edf9ead7a0c
barret <47966933+Barretem@users.noreply.github.com>
2019-12-30 12:17:27 +0800

    feat: 修改build脚本

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-set -e
-cd `dirname $0`
-echo "检查yarn"
-
-if ! which yarn -v &> /dev/null ;then
-  echo "安装yarn"
-  npm i -g yarn
-fi
-
 echo "build project"
 yarn --frozen-lockfile && yarn build
 
 echo "启动项目"
-yarn start
+yarn start:prod
