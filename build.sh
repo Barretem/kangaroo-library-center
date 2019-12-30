26d3f631bf41f78d55acf9083716eb2336e558f8
barret <47966933+Barretem@users.noreply.github.com>
2019-12-30 12:02:01 +0800

    feat: 调整build脚本

--- build.sh
+++ build.sh
@@ -8,26 +8,6 @@ if ! which yarn -v &> /dev/null ;then
   npm i -g yarn
 fi
 
-if $NODE_ENV === 'production' ;then
-  rm -rf ../production
-  mkdir ../production
-  cp ./* ../production/
-  cd ../production/
-  echo "拷贝对应的环境文件"
-  echo $PWD
-  cd "${$PWD}/source"
-  cp "${$PWD}/production.env" "${$PWD}/source/production.env"
-else
-  rm -rf ../development
-  mkdir ../development
-  cp ./* ../development/
-  cd ../development/
-  echo "拷贝对应的环境文件"
-  echo $PWD
-  cd "${$PWD}/source"
-  cp "${$PWD}/development.env" "${$PWD}/source/development.env"
-fi
-
 echo "build project"
 yarn --frozen-lockfile && yarn build
 
